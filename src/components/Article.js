7711ca76b9ec70e40806c9d42f1e7c0ce0d72661
mustafaguldag <guldag.mustafaa@gmail.com>
2021-06-11 11:33:54 +0300

    commit tree 8476332035e61b4490ab5fec188c0c464be5af18

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,8 +3,7 @@ import React from "react";
 function Article() {
   return (
     <div>
-      {/* uncomment the line below to pass the test */}
-      {/* please pass this test */}
+       <p>please pass this test</p> 
     </div>
   );
 }
